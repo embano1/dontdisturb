d86440b18d8dce58321865212dc0982ab9b1f21a
Michael Gasch <embano1@live.com>
2017-12-19 10:02:07 +0100

    Update readme and add error handling

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ Small program to enable "Do not disturb" mode on **OSX** for X minutes,
 since OSX only allows for setting this for the rest of the day.
 
 ## Install
+# Use a Release
 
-`go get github.com/embano1/dontdisturb`
+Grab the latest release from [here](https://github.com/embano1/dontdisturb/releases).
+
+# Build from Source
+
+`go get -u github.com/embano1/dontdisturb`
 
 ## Run (default: 15min)
 
--- main.go
+++ main.go
@@ -14,6 +14,7 @@ var min int
 
 func main() {
 
+	// Make sure we enable Notifitcations when the program exits
 	defer disableDND()
 	flag.IntVar(&min, "t", 15, "Time in minutes for 'Do not disturb' mode")
 	flag.Parse()
@@ -43,16 +44,42 @@ func main() {
 func enableDND() {
 	fmt.Printf("Turning on 'Do not disturb' mode for %d minutes\n", min)
 
-	// we don´t do error checking
-	_, _ = exec.Command("sh", "-c", "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true").Output()
-	_, _ = exec.Command("sh", "-c", "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date '`date -u +'%Y-%m-%d %H:%M:%S +000'`'").Output()
-	_, _ = exec.Command("sh", "-c", "killall NotificationCenter").Output()
+	cmd := exec.Command("sh", "-c", "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: could not set doNotDisturb to true: %v", err)
+		os.Exit(1)
+	}
+
+	cmd = exec.Command("sh", "-c", "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date \"`date -u +\"%Y-%m-%d %H:%M:%S +000\"`\"")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: could not write time to ~/Library/Preferences/ByHost/com.apple.notificationcenterui: %v", err)
+		os.Exit(1)
+	}
+
+	cmd = exec.Command("sh", "-c", "killall NotificationCenter")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: could not reset NotificationCenter: %v", err)
+		os.Exit(1)
+	}
 }
 
 func disableDND() {
-	// we don´t do error checking
-	_, _ = exec.Command("sh", "-c", "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean false").Output()
-	_, _ = exec.Command("sh", "-c", "killall NotificationCenter").Output()
+	cmd := exec.Command("sh", "-c", "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean false")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: could not set doNotDisturb to false: %v", err)
+		os.Exit(1)
+	}
+
+	cmd = exec.Command("sh", "-c", "killall NotificationCenter")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: could not reset NotificationCenter: %v", err)
+		os.Exit(1)
+	}
 
 	fmt.Println("Turning off 'Do not disturb' mode")
 }
